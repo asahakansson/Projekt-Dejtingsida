34d82e40b3534126af1c8dbcd02d1093244e72eb
åsa <aasa.haakansson@gmail.com>
2019-01-03 12:28:20 +0100

    Add project files.

---
+++ Projekt Dejtingsida/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projekt_Dejtingsida.Startup))]
+namespace Projekt_Dejtingsida
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

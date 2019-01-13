5b42d24fccacc9247a16c447b544dd83cca9d415
Björn Jonsson <bjorn_jonsson2@hotmail.com>
2019-01-13 09:49:05 +0100

    Partial view och messageviewmodel

---
+++ Projekt Dejtingsida/Models/MessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projekt_Dejtingsida.Models
+{
+    public class MessageViewModel
+    {
+        public List<MessageModel> Messages { get; set; }
+    }
+}

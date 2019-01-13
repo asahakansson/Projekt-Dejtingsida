d0ea4c9a75d061cd0c4bca8c9fb9cd1b1a926c37
åsa <aasa.haakansson@gmail.com>
2019-01-13 19:30:49 +0100

    Lagt till tom api-controller

---
+++ Projekt Dejtingsida/Controllers/GuestBookApiController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Projekt_Dejtingsida.Controllers
+{
+    public class GuestBookApiController : ApiController
+    {
+    }
+}
